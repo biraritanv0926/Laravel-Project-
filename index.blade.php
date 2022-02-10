299a2dd5e99bcdc73427cdcdd09f044d58aad147
Tanvi Birari <94899020+biraritanv0926@users.noreply.github.com>
2022-02-10 11:07:53 +0530

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title','Blog Posts')
+
+@section('content')
+{{-- @each('posts.partials.post', $post, 'post') --}}
+@forelse ($post as $key =>$post)
+    @include('posts.partials.post', [])
+@empty
+no post found
+@endforelse
+@endsection
