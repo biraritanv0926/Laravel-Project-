3991efa22b8d207c1dd076cbdc25a142b7b4e40f
Tanvi Birari <94899020+biraritanv0926@users.noreply.github.com>
2022-02-14 18:05:02 +0530

    Add files via upload

--- BlogPost.php
+++ BlogPost.php
@@ -10,6 +10,6 @@ class BlogPost extends Model
     protected $fillable = ['title','content'];
     public function comments()
     {
-        return $this->hasMany('App\Comment');
+        return $this->hasMany('App\Models\Comment');
     }
 }
