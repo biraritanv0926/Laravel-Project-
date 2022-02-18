5687824c5ef456ca27f23225692ce0aec3634a7c
Tanvi Birari <94899020+biraritanv0926@users.noreply.github.com>
2022-02-18 17:49:17 +0530

    Add files via upload

--- BlogPostFactory.php
+++ BlogPostFactory.php
@@ -2,14 +2,14 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\BlogPost::class, function (Faker $faker) {
+$factory->define(App\Models\BlogPost::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(10),
         'content' => $faker->paragraphs(5, true)
     ];
 });
 
-$factory->state(App\BlogPost::class, 'new-title', function (Faker $faker) {
+$factory->state(App\Models\BlogPost::class, 'new-title', function (Faker $faker) {
     return [
         'title' => 'New title',
     ];
