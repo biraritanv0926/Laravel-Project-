bb2968574701284f9b4e7df1eced16a3afa96517
Tanvi Birari <94899020+biraritanv0926@users.noreply.github.com>
2022-02-25 18:38:48 +0530

    Update Comment.php

--- Comment.php
+++ Comment.php
@@ -1,14 +1,32 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Taggable;
 
 class Comment extends Model
 {
-    public function blogPost()
+    use SoftDeletes, Taggable;
+
+    protected $fillable = ['user_id', 'content'];
+
+    protected $hidden = ['deleted_at', 'commentable_type', 'commentable_id', 'user_id'];
+
+    public function commentable()
     {
-        return $this->belongsTo('App\BlogPost');
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function scopeLatest(Builder $query)
+    {
+        return $query->orderBy(static::CREATED_AT, 'desc');
     }
 }
